4b42ec8f936a7571e214503318712432cd94dfc2
Jymit <12527842+Jymit@users.noreply.github.com>
2018-12-06 10:18:50 +0000

    Create unshortenMe.sh

---
+++ unshortenMe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ! -z "$1" ]]; then
+  url="$1";
+fi;
+
+[ $# -eq 0 ] && { echo "Usage: $0 <shortened url>"; exit 1; }
+
+regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
+
+if [[ $url =~ $regex ]]
+then
+  echo -e "\n  Shortened URL:  $1"
+  result=`curl -sIL $1 | grep Location | awk '{print $2}'`
+  echo "Unshortened URL:  $result"
+else
+  echo "Invalid URL"
+fi
