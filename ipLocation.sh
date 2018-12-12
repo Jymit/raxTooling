bc60e27e4b45cc4f62c1e954b75ec9a711a575c9
Jymit <12527842+Jymit@users.noreply.github.com>
2018-12-12 11:01:39 +0000

    Delete ipLocation.sh

--- ipLocation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-#Setup and get API access @ https://ipstack.com
-#
-echo "The public IP is:" $(curl -s icanhazip.com)
-curl -s http://api.ipstack.com/check?access_key=<myApiKeyHere> |jq . 
